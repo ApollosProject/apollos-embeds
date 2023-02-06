4efa4b6d9eb43bb27587b1f5b16f05d9c8551f21
Jordan Wade <jjwade31@gmail.com>
2023-02-06 16:22:33 -0600

    Do not render Auth if token and step is success

--- src/embeds/Auth.js
+++ src/embeds/Auth.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { AuthManager } from '../components';
 import { AUTH_TOKEN_KEY } from '../config/keys';
+import { useAuth } from '../providers/AuthProvider';
+import authSteps from '../components/Auth/authSteps';
 
 const Auth = (props) => {
+  const [{ step }] = useAuth();
+
   if (
     process.env.REACT_APP_ENABLE_AUTH === 'true' &&
     !window.localStorage.getItem(AUTH_TOKEN_KEY)
   ) {
     return <AuthManager />;
+  }
+  if (
+    step === authSteps.Success &&
+    window.localStorage.getItem(AUTH_TOKEN_KEY)
+  ) {
+    return null;
   }
   return null;
 };
