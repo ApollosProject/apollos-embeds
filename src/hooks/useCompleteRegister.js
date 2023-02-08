654751c5bfda681d3ae599d71808e37a52c9e738
Jordan Wade <jjwade31@gmail.com>
2023-02-08 14:51:14 -0600

    Add useCompleteRegister hook

---
+++ src/hooks/useCompleteRegister.js
@@ -0,0 +1,15 @@
+import { gql, useMutation } from '@apollo/client';
+
+export const COMPLETE_REGISTER = gql`
+  mutation completeRegistration($mergeProfileId: ID) {
+    completeRegistration(mergeProfileId: $mergeProfileId) {
+      id
+    }
+  }
+`;
+
+function useCompleteRegister(options = {}) {
+  return useMutation(COMPLETE_REGISTER, options);
+}
+
+export default useCompleteRegister;
