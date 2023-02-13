dab38cb60b0f2f11199de0c22bf85c88cb487616
Jordan Wade <jjwade31@gmail.com>
2023-02-13 16:39:24 -0600

    Update AuthIdentity UI

--- src/components/Auth/AuthIdentity.js
+++ src/components/Auth/AuthIdentity.js
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import authSteps from './authSteps';
 import { useForm, useRequestLogin, useRequestRegister } from '../../hooks';
 import { update as updateAuth, useAuth } from '../../providers/AuthProvider';
-import { Box, Button, Card, H2, H5, Input } from '../../ui-kit';
+import { Button, Input, SmallSystemText, Box } from '../../ui-kit';
 import { validateEmail, validatePhoneNumber } from '../../utils';
+import AuthLayout from './AuthLayout';
 
 const AuthIdentity = () => {
   const [status, setStatus] = useState('IDLE');
   const [error, setError] = useState(null);
   const [state, dispatch] = useAuth();
+  const [checked, setChecked] = useState(false);
+
+  const onChange = () => setChecked(!checked);
 
   const onSuccess = ({ identity, type, userExists, userProfile, nextStep }) => {
     setStatus('SUCCESS');
@@ -80,41 +84,92 @@ const AuthIdentity = () => {
   const isLoading = status === 'LOADING';
 
   return (
-    <Box
-      position="fixed"
-      top="0"
-      bottom="0"
-      left="0"
-      right="0"
-      overflow="scroll"
-      zIndex={9999}
-      justifyContent="center"
-      display="flex"
-      alignItems="center"
-      backgroundColor="neutral.gray6"
+    <AuthLayout
+      heading={
+        state.type === 'login' ? 'Welcome back!' : 'We’re glad you’re here.'
+      }
+      subHeading={
+        state.type === 'login'
+          ? 'Use the same phone number or email that you used when logging into the app before.'
+          : 'If you’ve attended with us before, we’ll sync your history and help personalize what’s next.'
+      }
     >
-      <Card p="l" display="flex" flexDirection="column" width="440px">
-        <H2>We’re glad you’re here.</H2>
-        <H5>
-          If you’ve attended with us before, we’ll sync your history and help
-          personalize what’s next.
-        </H5>
-        <Input
-          id="identity"
-          placeholder="Phone or Email"
-          value={values.identity || ''}
-          handleOnChange={(text) => setFieldValue('identity', text)}
-          required
-          autoFocus={true}
-          error={error?.identity}
+      <Input
+        id="identity"
+        placeholder="Phone or Email"
+        value={values.identity || ''}
+        handleOnChange={(text) => setFieldValue('identity', text)}
+        required
+        autoFocus={true}
+        error={error?.identity}
+        mb="xs"
+      />
+      <Box display="flex" mb="base" alignItems="baseline">
+        <Box
+          as="input"
+          id="policy"
+          type="radio"
+          mr="xxs"
+          key="policy"
+          value={checked}
+          checked={checked === true}
+          onClick={onChange}
+          onChange={onChange}
         />
-        <Button
-          title={isLoading ? 'Submitting...' : 'Submit'}
-          onClick={handleSubmit}
-          mt="base"
-        />
-      </Card>
-    </Box>
+
+        <SmallSystemText color="text.secondary">
+          I agree to the{' '}
+          <Box display="inline" fontWeight={700}>
+            terms of use
+          </Box>{' '}
+          and{' '}
+          <Box display="inline" fontWeight={700}>
+            privacy policy
+          </Box>{' '}
+          laid out by this church.
+        </SmallSystemText>
+      </Box>
+      <SmallSystemText fontWeight={700} color="text.secondary" mb="s">
+        We’ll text or email you a code to continue.
+      </SmallSystemText>
+      <Button
+        title={isLoading ? 'Submitting...' : 'Next'}
+        onClick={handleSubmit}
+        mt="base"
+        mb="xs"
+      />
+      {state.type === 'login' ? (
+        <>
+          <SmallSystemText color="text.secondary" textAlign="center">
+            Already have an account?
+            <span>
+              {' '}
+              <Button
+                display="inline"
+                title="Log in →"
+                type="link"
+                size="micro"
+              />
+            </span>
+          </SmallSystemText>
+        </>
+      ) : (
+        <>
+          <SmallSystemText color="text.secondary" textAlign="center">
+            Having trouble?
+            <span>
+              {' '}
+              <Button
+                display="inline"
+                title="Create Account →"
+                type="link"
+                size="micro"
+              />
+            </span>
+          </SmallSystemText>
+        </>
+      )}
+    </AuthLayout>
   );
 };
 
