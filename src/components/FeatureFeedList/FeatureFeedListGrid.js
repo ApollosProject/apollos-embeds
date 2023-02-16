9dd7be97667ff78d61ce0af64600d18fb8ddc10e
Jordan Wade <jjwade31@gmail.com>
2023-02-16 09:37:24 -0600

    Fix FeatureFeedListGrid

--- src/components/FeatureFeedList/FeatureFeedListGrid.js
+++ src/components/FeatureFeedList/FeatureFeedListGrid.js
@@ -1,47 +1,39 @@
-// import { useNavigation } from '@apollosproject/tv-shared/router';
-// import { ContentItemCard } from '@apollosproject/tv-shared/ui-kit';
-// import { getPathFromType } from '@apollosproject/tv-shared/utils';
-import PropTypes from 'prop-types';
 import React from 'react';
+
+import { useNavigate } from 'react-router-dom';
 import { withTheme } from 'styled-components';
 
-import { Box, H3 } from '../../ui-kit';
+import { getURLFromType } from '../../utils';
+import { Box, ContentCard, H3 } from '../../ui-kit';
 
 function FeatureFeedListGrid(props = {}) {
-  // const router = useNavigation();
+  const navigate = useNavigate();
 
   // const [viewWidth, setViewWidth] = React.useState(0);
   // const boxWidth = viewWidth * 0.25 - 66;
 
-  // const handleActionPress = (item) => {
-  //   router.push(getPathFromType(item.relatedNode), {
-  //     itemId: item.relatedNode.id,
-  //   });
-  // };
+  const handleActionPress = (item) => {
+    navigate({
+      pathname: '/',
+      search: `?id=${getURLFromType(item.relatedNode)}`,
+    });
+  };
 
   return (
-    <Box pb="l" backgroundColor="#323233" {...props}>
-      <H3 mb="xs">
-        {'This is a grid of Feature Feed items for view more clicks'}
-      </H3>
-      {/* {props.feature?.cards?.map((item, index) => (
-        <Box key={index} width="200px" mr="16px">
-          <img
-            alt="Content Title"
-            src={item.coverImage?.sources[0]?.uri}
-            width="100%"
+    <Box pb="l" {...props}>
+      <Box display="grid" gridTemplateColumns="repeat(3, 1fr)" gridGap="20px">
+        {props.data.features[0]?.cards?.map((item, index) => (
+          <ContentCard
+            key={item.title}
+            image={item.coverImage}
+            title={item.title}
+            summary={item.summary}
+            onClick={() => handleActionPress(item)}
           />
-        </Box>
-      ))} */}
+        ))}
+      </Box>
     </Box>
   );
 }
-
-FeatureFeedListGrid.propTypes = {
-  data: PropTypes.shape({
-    data: PropTypes.arrayOf(PropTypes.shape({})),
-  }),
-  loading: PropTypes.bool,
-};
 
 export default withTheme(FeatureFeedListGrid);
