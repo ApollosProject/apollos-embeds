55b905b4aeb00af596aec63306a264d923de534f
Jordan Wade <jjwade31@gmail.com>
2023-02-16 14:21:36 -0600

    Simpligy Auth embed

--- src/embeds/Auth.js
+++ src/embeds/Auth.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import { AuthManager } from '../components';
 import { AUTH_TOKEN_KEY } from '../config/keys';
-import { useAuth } from '../providers/AuthProvider';
-import authSteps from '../components/Auth/authSteps';
 
-const Auth = (props) => {
-  const [{ step }] = useAuth();
-
-  if (
-    process.env.REACT_APP_ENABLE_AUTH === 'false' ||
-    (step === authSteps.Success && window.localStorage.getItem(AUTH_TOKEN_KEY))
-  ) {
+const Auth = () => {
+  if (window.localStorage.getItem(AUTH_TOKEN_KEY)) {
     return null;
   }
   return <AuthManager />;
