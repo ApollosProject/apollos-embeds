a5eaf03332c3cd979e669130bb987ecf21af2dea
Jordan Wade <jjwade31@gmail.com>
2023-03-03 15:27:10 -0600

    fix percent watched bug

--- src/utils/getPercentWatched.js
+++ src/utils/getPercentWatched.js
@@ -1,5 +1,4 @@
-import clamp from 'lodash/clamp';
-import floor from 'lodash/floor';
+import round from 'lodash/round';
 import isNil from 'lodash/isNil';
 
 // Given a VideoMedia node, return a percentage between 0–1 representing how much
@@ -11,7 +10,7 @@ export default function getUserPercentComplete({ duration, userProgress }) {
   const hasDuration = !isNil(duration);
 
   if (hasPlayhead && hasDuration) {
-    return clamp(floor(playhead / duration, 2), 0, 1);
+    return round(playhead / duration, 2);
   }
 
   return null;
