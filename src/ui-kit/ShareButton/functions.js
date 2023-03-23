61136c5482860d602dccfd7f82af53c68e72c8f4
Brian <brian@differential.com>
2023-03-23 16:25:50 -0400

    update facebook function

--- src/ui-kit/ShareButton/functions.js
+++ src/ui-kit/ShareButton/functions.js
@@ -1,15 +1,15 @@
-export function shareLinkOnFacebook(link) {
-  const url = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
-    link
-  )}`;
-  window.open(url, '_blank', 'toolbar=0,location=0,menubar=0');
-}
-
 export function shareLinkByEmail(link) {
   const subject = encodeURIComponent('Check this out on Apollos');
   const body = encodeURIComponent(`Hey, check out this post: ${link}`);
   const mailtoLink = `mailto:?subject=${subject}&body=${body}`;
   window.location.href = mailtoLink;
+}
+
+export function shareLinkOnFacebook(link) {
+  const url = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+    link
+  )}`;
+  window.open(url, '_blank', 'toolbar=0,location=0,menubar=0');
 }
 
 export function shareLinkOnTwitter(link) {
