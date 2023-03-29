46583426405bfcd2b364869a031de3410157c4aa
Jordan Wade <jjwade31@gmail.com>
2023-03-29 15:53:13 -0500

    Add HorizontalMediaListFeature component

---
+++ src/components/FeatureFeed/Features/HorizontalMediaListFeature.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import { useNavigate, useSearchParams, useLocation } from 'react-router-dom';
+
+import { getURLFromType } from '../../../utils';
+import { ContentCard, Box, H3, systemPropTypes, Button } from '../../../ui-kit';
+import {
+  add as addBreadcrumb,
+  useBreadcrumb,
+} from '../../../providers/BreadcrumbProvider';
+
+import Carousel from 'react-multi-carousel';
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    partialVisibilityGutter: 30,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    partialVisibilityGutter: 30,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    partialVisibilityGutter: 30,
+  },
+};
+
+const MediaItemWithLiveStreamManager = ({ item, boxWidth, onPress }) => {
+  const isLive = useLivestreamIsActive(item?.relatedNode);
+
+  return (
+    <MediaItem
+      isLive={isLive}
+      coverImage={item.coverImage}
+      title={item.title}
+      duration={get(item, 'relatedNode.videos[0].duration', null)}
+      userProgress={get(item, 'relatedNode.videos[0].userProgress', null)}
+      onPress={onPress}
+      width={boxWidth}
+      contentId={item.relatedNode.id}
+      height="100%"
+      last={item?.last ?? false}
+      px="xs"
+    />
+  );
+};
+
+{
+  /* <ContentCard
+key={item.title}
+image={item.coverImage}
+title={item.title}
+summary={item.summary}
+onClick={() => handleActionPress(item)}
+videoMedia={item.relatedNode?.videos[0]}
+/> */
+}
+
+function HorizontalMediaListFeature(props = {}) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [state, dispatch] = useBreadcrumb();
+  const handleActionPress = (item) => {
+    dispatch(
+      addBreadcrumb({
+        url: `?id=${getURLFromType(item.relatedNode)}`,
+        title: item.relatedNode?.title,
+      })
+    );
+    setSearchParams(`?id=${getURLFromType(item.relatedNode)}`);
+  };
+
+  const handlePrimaryActionPress = () => {
+    dispatch(
+      addBreadcrumb({
+        url: `?id=${getURLFromType(props?.feature?.primaryAction.relatedNode)}`,
+        title: props?.feature?.title,
+      })
+    );
+    setSearchParams(
+      `?id=${getURLFromType(props?.feature?.primaryAction.relatedNode)}`
+    );
+  };
+
+  return (
+    <Box pb="l" {...props}>
+      <Box display="flex">
+        <H3 flex="1" mb="xs">
+          {props.feature.title}
+        </H3>
+        {props?.feature?.cards?.length >= 5 && props?.feature?.primaryAction ? (
+          <Button
+            title="View All >"
+            type="link"
+            onClick={handlePrimaryActionPress}
+          />
+        ) : null}
+      </Box>
+      <Carousel
+        swipeable={true}
+        draggable={false}
+        showDots={false}
+        responsive={responsive}
+        ssr={true} // means to render carousel on server-side.
+        infinite={true}
+        autoPlaySpeed={1000}
+        keyBoardControl={true}
+      >
+        {props.feature?.cards?.map((item, index) => (
+          <MediaItemWithLiveStreamManager item={item} />
+        ))}
+      </Carousel>
+    </Box>
+  );
+}
+
+HorizontalMediaListFeature.propTypes = {
+  ...systemPropTypes,
+};
+
+export default HorizontalMediaListFeature;
--- src/components/FeatureFeed/Features/index.js
+++ src/components/FeatureFeed/Features/index.js
@@ -1,11 +1,13 @@
+import ButtonFeature from './ButtonFeature';
 import HeroListFeature from './HeroListFeature';
 import HorizontalCardListFeature from './HorizontalCardListFeature';
+import HorizontalMediaListFeature from './HorizontalMediaListFeature';
 import VerticalCardListFeature from './VerticalCardListFeature';
-import ButtonFeature from './ButtonFeature';
 
 export {
+  ButtonFeature,
   HeroListFeature,
   HorizontalCardListFeature,
+  HorizontalMediaListFeature,
   VerticalCardListFeature,
-  ButtonFeature,
 };
