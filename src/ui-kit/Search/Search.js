f38059cfefda7629d775e57139ad0110f9573258
Brian <brian@differential.com>
2023-05-04 16:20:40 -0400

    add textPrompt logic

--- src/ui-kit/Search/Search.js
+++ src/ui-kit/Search/Search.js
@@ -6,21 +6,55 @@ import { Box, Button } from '../../ui-kit';
 import Styled from './Search.styles';
 import { User, CaretDown, MagnifyingGlass } from 'phosphor-react';
 import { useCurrentUser } from '../../hooks';
+import { first } from 'lodash';
+import { te } from 'date-fns/locale';
 
 const Search = (props = {}) => {
   const [showDropdown, setShowDropdown] = useState(false);
+  const [showTextPrompt, setShowTextPrompt] = useState(true);
+  const [inputValue, setInputValue] = useState('');
   const { currentUser } = useCurrentUser();
-  const firstName = currentUser?.profile?.firstName;
-  // console.log(currentUser);
+  const firstName = currentUser?.profile?.firstName || '';
+
+  const textPrompt = (
+    <Styled.TextPrompt>
+      {firstName === '' ? (
+        <Box>
+          <strong>Hey!</strong>
+        </Box>
+      ) : (
+        <Box>
+          <strong>Hey {firstName}!&nbsp; </strong>
+        </Box>
+      )}
+      <Box>Find what's next</Box>
+    </Styled.TextPrompt>
+  );
 
   const handleInputFocus = () => {
     setShowDropdown(true);
-    console.log('focus');
+    setShowTextPrompt(false);
   };
 
   const handleInputBlur = () => {
     setShowDropdown(false);
-    console.log('blur');
+    if (inputValue.trim() === '') {
+      setShowTextPrompt(true);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setInputValue(value);
+
+    if (value.trim() === '') {
+      // Input is empty, do something
+      console.log('empty');
+      console.log(value);
+    } else {
+      // Input is not empty, do something else
+      console.log(value);
+    }
   };
 
   return (
@@ -31,7 +65,15 @@ const Search = (props = {}) => {
             <Styled.SearchIcon>
               <MagnifyingGlass size={18} weight="bold" color="white" />
             </Styled.SearchIcon>
-            <Styled.Input onFocus={handleInputFocus} onBlur={handleInputBlur} />
+            <Box width="100%" position="relative">
+              <Styled.Input
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+                onChange={handleInputChange}
+                value={inputValue}
+              />
+              {showTextPrompt ? textPrompt : null}
+            </Box>
           </Styled.InterfaceWrapper>
           <CaretDown size={9} weight="bold" />
         </Styled.Interface>
@@ -39,7 +81,7 @@ const Search = (props = {}) => {
           <User size={18} color="white" weight="bold" />
         </Styled.Profile>
       </Styled.Wrapper>
-      {showDropdown ? <Box>Hello</Box> : null}
+      {showDropdown ? <Styled.Dropdown>Hello</Styled.Dropdown> : null}
     </Box>
   );
 };
