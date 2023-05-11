b1efd5aafc23a6a5edb56f54e9419946439428a2
Jordan Wade <jjwade31@gmail.com>
2023-05-11 09:30:09 -0500

    Initial setup of ProfileDetails component

---
+++ src/components/Profile/ProfileDetails.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import { useForm, useUpdateProfileFields, useCurrentUser } from '../../hooks';
+import { Box, Button, Input } from '../../ui-kit';
+
+function upperFirst(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function ProfileDetails(props = {}) {
+  const [status, setStatus] = useState('IDLE');
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState(null);
+
+  const [updateProfileFields] = useUpdateProfileFields();
+
+  const { currentUser } = useCurrentUser();
+
+  const { firstName, lastName } = user;
+
+  useEffect(() => {
+    setUser(currentUser);
+  }, [currentUser]);
+
+  const onError = (e) => {
+    setStatus('ERROR');
+    setError(e);
+  };
+
+  const { values, handleSubmit, setFieldValue } = useForm(async () => {
+    setStatus('LOADING');
+
+    const userProfile = Object.keys(values).map((key) => ({
+      field: upperFirst(key),
+      value: values[key],
+    }));
+
+    try {
+      await updateProfileFields({ variables: { input: userProfile } });
+    } catch (e) {
+      onError(e);
+      console.log(JSON.stringify(e));
+    }
+  });
+
+  const isLoading = status === 'LOADING';
+
+  return (
+    <>
+      <Box as="form" id="editProfile" action="" onSubmit={handleSubmit}>
+        <>
+          <Box mb="l">
+            <Box mb="base">
+              <Input
+                id="firstName"
+                placeholder="First Name"
+                value={values.firstName || firstName || ''}
+                handleOnChange={(text) => setFieldValue('firstName', text)}
+                required
+                error={error?.identity}
+              />
+            </Box>
+
+            <Box mb="base">
+              <Input
+                id="lastName"
+                placeholder="Last Name"
+                value={values.lastName || lastName || ''}
+                handleOnChange={(text) => setFieldValue('lastName', text)}
+                required
+                error={error?.identity}
+              />
+            </Box>
+            <Button
+              title={`Finish${isLoading ? 'ing...' : ''}`}
+              onClick={handleSubmit}
+              disabled={
+                !user || !(values.firstName && values.lastName) || isLoading
+              }
+            />
+          </Box>
+        </>
+      </Box>
+    </>
+  );
+}
+
+ProfileDetails.propTypes = {
+  currentPerson: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
+};
+
+export default ProfileDetails;
