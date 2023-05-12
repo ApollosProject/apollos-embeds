de00867e59a508d30657544bfd0efc02321358cd
Jordan Wade <jjwade31@gmail.com>
2023-05-12 15:30:07 -0500

    Add search query hook

---
+++ src/hooks/useSearchQuery.js
@@ -0,0 +1,45 @@
+import { gql, useLazyQuery } from '@apollo/client';
+
+export const SEARCH = gql`
+  query search($query: String!, $first: Int, $after: String) {
+    search(query: $query, first: $first, after: $after) {
+      totalCount
+      pageInfo {
+        startCursor
+        endCursor
+      }
+      edges {
+        title
+        summary
+        coverImage {
+          name
+          sources {
+            uri
+          }
+        }
+        cursor
+        node {
+          ... on ContentItem {
+            id
+            __typename
+          }
+        }
+      }
+    }
+  }
+`;
+
+function useSearchQuery(options = {}) {
+  const [search, query] = useLazyQuery(SEARCH, {
+    ...options,
+  });
+  return [
+    search,
+    {
+      contentItems: query?.data?.search || {},
+      ...query,
+    },
+  ];
+}
+
+export default useSearchQuery;
