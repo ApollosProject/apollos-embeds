1b5219696eba885ee22cb2529f4b7dd9a1c9b0d1
Jordan Wade <jjwade31@gmail.com>
2023-05-18 16:53:42 -0500

    Add useUploadProfileImage hook

---
+++ src/hooks/useUploadProfileImage.js
@@ -0,0 +1,17 @@
+import { gql, useMutation } from '@apollo/client';
+
+export const UPLOAD_PROFILE_IMAGE = gql`
+  mutation uploadProfileImage($file: Upload!, $size: Int!) {
+    uploadProfileImage(file: $file, size: $size) {
+      photo {
+        uri
+      }
+    }
+  }
+`;
+
+function useUploadProfileImage(options = {}) {
+  return useMutation(UPLOAD_PROFILE_IMAGE, options);
+}
+
+export default useUploadProfileImage;
