c361475dbc3bf439c69d839cd4c9977d88edcabb
Jordan Wade <jjwade31@gmail.com>
2023-05-19 13:23:01 -0500

    Add id to upload image

--- src/hooks/useUploadProfileImage.js
+++ src/hooks/useUploadProfileImage.js
@@ -3,6 +3,7 @@ import { gql, useMutation } from '@apollo/client';
 export const UPLOAD_PROFILE_IMAGE = gql`
   mutation uploadProfileImage($file: Upload!, $size: Int!) {
     uploadProfileImage(file: $file, size: $size) {
+      id
       photo {
         uri
       }
