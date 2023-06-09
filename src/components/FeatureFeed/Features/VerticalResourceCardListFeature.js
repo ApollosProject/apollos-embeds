0d45fc6b14b45c7fb6273f97b2992df2f5e4d1b8
Brian <brian@differential.com>
2023-06-09 17:48:06 -0400

    initial vertialresourcecardlist

---
+++ src/components/FeatureFeed/Features/VerticalResourceCardListFeature.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+import { getURLFromType } from '../../../utils';
+import { Box, systemPropTypes, ResourceCard } from '../../../ui-kit';
+import Styled from './VerticalResourceCardListFeature.styles';
+import { ArrowUpRight } from 'phosphor-react';
+
+function VerticalResourceCardListFeature(props = {}) {
+  const temp = [
+    { title: 'Title', subtitle: 'Subtitle' },
+    { title: 'Title', subtitle: 'Subtitle' },
+    { title: 'Title', subtitle: 'Subtitle' },
+  ];
+  return (
+    <Styled.List>
+      {temp.map((item) => {
+        return (
+          <ResourceCard
+            title={item.title}
+            subtitle={item.subtitle}
+            background="none"
+          />
+        );
+      })}
+    </Styled.List>
+  );
+}
+
+VerticalResourceCardListFeature.propTypes = {
+  ...systemPropTypes,
+};
+
+export default VerticalResourceCardListFeature;
