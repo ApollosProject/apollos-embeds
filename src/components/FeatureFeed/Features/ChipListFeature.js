5680fcaec27e81832c2c9f8f1a3223a019257412
Brian <brian@differential.com>
2023-06-12 14:16:20 -0400

    update chiplist feature

--- src/components/FeatureFeed/Features/ChipListFeature.js
+++ src/components/FeatureFeed/Features/ChipListFeature.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import { getURLFromType } from '../../../utils';
-import { systemPropTypes } from '../../../ui-kit';
+import { systemPropTypes, Box } from '../../../ui-kit';
 import Styled from './ChipListFeature.styles';
 import { ArrowUpRight } from 'phosphor-react';
 
 import { useNavigate } from 'react-router-dom';
 
 function ChipListFeature(props = {}) {
+  const items = ['Give', 'Service Times', 'Childcare', 'Chip', 'Test'];
   const navigate = useNavigate();
   const handleActionPress = () => {
     navigate({
@@ -17,34 +18,39 @@ function ChipListFeature(props = {}) {
   };
 
   return (
-    <Styled.List>
-      {props.items.map((label, index) => {
-        if (index === 0) {
+    <Box>
+      <Box padding="xs" fontWeight="600" color="base.gray" id="results">
+        Quick Links
+      </Box>
+      <Styled.List>
+        {items.map((label, index) => {
+          if (index === 0) {
+            return (
+              <Styled.Chip ml="xs">
+                <ArrowUpRight size={20} weight="bold" color="#8E8E93" />
+                <span>{label}</span>
+              </Styled.Chip>
+            );
+          }
+
+          if (index === items.length - 1) {
+            return (
+              <Styled.Chip mr="xs">
+                <ArrowUpRight size={20} weight="bold" color="#8E8E93" />
+                <span>{label}</span>
+              </Styled.Chip>
+            );
+          }
+
           return (
-            <Styled.Chip ml="xs">
+            <Styled.Chip>
               <ArrowUpRight size={20} weight="bold" color="#8E8E93" />
               <span>{label}</span>
             </Styled.Chip>
           );
-        }
-
-        if (index === props.items.length - 1) {
-          return (
-            <Styled.Chip mr="xs">
-              <ArrowUpRight size={20} weight="bold" color="#8E8E93" />
-              <span>{label}</span>
-            </Styled.Chip>
-          );
-        }
-
-        return (
-          <Styled.Chip>
-            <ArrowUpRight size={20} weight="bold" color="#8E8E93" />
-            <span>{label}</span>
-          </Styled.Chip>
-        );
-      })}
-    </Styled.List>
+        })}
+      </Styled.List>
+    </Box>
   );
 }
 
