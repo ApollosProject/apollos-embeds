4d93751d4599bd1bcab97ce658a418ba837b9b10
Jordan Wade <jjwade31@gmail.com>
2023-06-28 13:17:48 -0500

    Handle url parsing to load root feature

--- micro-service/src/App.js
+++ micro-service/src/App.js
@@ -3,6 +3,7 @@ import * as Sentry from '@sentry/react';
 import { FeatureFeed } from '@apollosproject/web-shared/embeds';
 import { AppProvider } from '@apollosproject/web-shared/providers';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import ErrorPage from './error-page';
 
 Sentry.init({
@@ -13,27 +14,31 @@ Sentry.init({
 });
 
 function App(props) {
+  const subdomain =
+    process.env.NODE_ENV === 'production'
+      ? window.location.hostname.split('.').slice(0, -2).join('.')
+      : window.location.hostname.split('.').slice(0, -1).join('.');
+  const churchSlug = subdomain.replace(/-/g, '_');
+
+  const searchParams = new URLSearchParams(window.location.search);
+  const _root = searchParams.get('root');
+
+  const [type, randomId] = _root?.split(/-(.*)/s) ?? [];
+
   const router = createBrowserRouter([
     {
       path: '/',
       element: (
-        <FeatureFeed
-          featureFeed={'FeatureFeed:5aae43e6-3526-4cd2-8dfe-771d2ce8a333'}
-          church={'cedar_creek'}
-        />
+        <FeatureFeed featureFeed={`${type}:${randomId}`} church={churchSlug} />
       ),
       errorElement: <ErrorPage />,
     },
   ]);
 
   // Widgets require a church slug to get the correct data
-  if ('cedar_creek') {
+  if (churchSlug) {
     return (
-      <AppProvider
-        church={'cedar_creek'}
-        modal="true"
-        searchFeed={'FeatureFeed:33b34f30-e57c-4c23-9f28-24d31959a3f4'}
-      >
+      <AppProvider church={churchSlug} modal="true">
         <RouterProvider router={router} />
       </AppProvider>
     );
