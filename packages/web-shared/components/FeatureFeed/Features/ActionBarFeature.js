bc12bd50f39830e689eef7b695031ae83694d5f8
Brian Lin <brian@differential.com>
2023-09-12 23:52:03 -0400

    initial action renders

--- packages/web-shared/components/FeatureFeed/Features/ActionBarFeature.js
+++ packages/web-shared/components/FeatureFeed/Features/ActionBarFeature.js
@@ -9,45 +9,11 @@ import { useNavigate } from 'react-router-dom';
 
 function ActionBarFeature(props = {}) {
   console.log(props);
-  if (props?.feature?.chips?.length === 0 || !props?.feature?.chips) {
-    return null;
-  }
-
   return (
-    <Box>
-      <Box padding="xs" fontWeight="600" color="base.gray" id="results">
-        {props.feature.title || props.feature.subtitle}
-      </Box>
-      <Styled.List>
-        {props.feature?.chips?.map(
-          ({ title, iconName, relatedNode }, index) => {
-            if (index === 0) {
-              return (
-                <Styled.Chip ml="xs" href={relatedNode.url} key={index}>
-                  <ArrowUpRight size={20} weight="bold" color="#8E8E93" />
-                  <span>{title}</span>
-                </Styled.Chip>
-              );
-            }
-
-            if (index === props.feature?.chips.length - 1) {
-              return (
-                <Styled.Chip mr="xs" href={relatedNode.url} key={index}>
-                  <ArrowUpRight size={20} weight="bold" color="#8E8E93" />
-                  <span>{title}</span>
-                </Styled.Chip>
-              );
-            }
-
-            return (
-              <Styled.Chip href={relatedNode.url} key={index}>
-                <ArrowUpRight size={20} weight="bold" color="#8E8E93" />
-                <span>{title}</span>
-              </Styled.Chip>
-            );
-          }
-        )}
-      </Styled.List>
+    <Box width="100%" display="flex">
+      {props.feature?.actions?.map((item, index) => (
+        <Box flex="1">hi</Box>
+      ))}
     </Box>
   );
 }
