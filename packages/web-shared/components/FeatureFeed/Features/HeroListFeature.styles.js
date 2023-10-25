21a77ee0775f466822b9e241323fdb771796dd1c
jordanwade <jjwade31@gmail.com>
2023-10-25 11:51:19 -0500

    Tweak responsive hero list feature

--- packages/web-shared/components/FeatureFeed/Features/HeroListFeature.styles.js
+++ packages/web-shared/components/FeatureFeed/Features/HeroListFeature.styles.js
@@ -9,10 +9,6 @@ const twoCardlayout = ({ length }) => {
   if (length === 2) {
     return css`
       grid-template-columns: repeat(2, 1fr);
-
-      @media screen and (min-width: ${themeGet('breakpoints.md')}) {
-        margin-right: -11px;
-      }
     `;
   } else {
     return null;
@@ -39,27 +35,30 @@ const Summary = withTheme(styled.div`
 
 const Container = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: auto;
-  grid-auto-rows: 1fr;
-  grid-auto-columns: 1fr;
-  ${twoCardlayout}
 
-  @media screen and (max-width: ${themeGet('breakpoints.lg')}) {
+  grid-template-rows: auto;
+  grid-gap: 20px;
+
+  grid-template-columns: repeat(1, 1fr);
+
+  @media screen and (min-width: ${themeGet('breakpoints.sm')}) {
     grid-template-columns: repeat(2, 1fr);
   }
-  @media screen and (max-width: ${themeGet('breakpoints.md')}) {
-    grid-template-columns: repeat(1, 1fr);
+  @media screen and (min-width: ${themeGet('breakpoints.md')}) {
+    grid-template-columns: repeat(3, 1fr);
+    ${twoCardlayout}
+  }
+  @media screen and (min-width: ${themeGet('breakpoints.lg')}) {
+    grid-template-columns: repeat(4, 1fr);
+    ${twoCardlayout}
   }
 `;
 
 const Item = styled.div`
-  margin-bottom: ${themeGet('space.l')};
   margin-right: 0;
 
   @media screen and (min-width: ${themeGet('breakpoints.md')}) {
     margin-bottom: 0;
-    margin-right: 11px;
   }
 `;
 
