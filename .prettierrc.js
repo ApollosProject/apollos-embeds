45ad49cc324297b32f72b2d037812861313f6ba6
jordanwade <jjwade31@gmail.com>
2023-11-30 12:44:43 -0600

    Align config with platforms prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
+  arrowParens: 'always', // custom for Apollos, RN defaults to 'avoid'
   bracketSameLine: false, // custom for Apollos, RN defaults to 'true'
   bracketSpacing: true, // custom for Apollos, RN defaults to 'false'
   singleQuote: true,
-  trailingComma: 'all', // custom for Apollos, RN defaults to 'all'
+  trailingComma: 'es5', // custom for Apollos, RN defaults to 'all'
   tabWidth: 2,
   proseWrap: 'preserve',
   printWidth: 100,
