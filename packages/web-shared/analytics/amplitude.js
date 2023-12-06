f04b7e171d1e9584ee8bec51b7131b55b8308ad1
Brian Lin <brian@differential.com>
2023-12-06 17:36:29 -0500

    add initial amplitude

--- packages/web-shared/analytics/amplitude.js
+++ packages/web-shared/analytics/amplitude.js
@@ -1,5 +1,34 @@
-import * as amplitude from '@amplitude/analytics-browser';
+import amplitudeJS from 'amplitude-js';
 
-export const init = amplitude.init(process.env.REACT_APP_AMPLITUDE_KEY, { defaultTracking: true });
+export const trackEvent = (eventName, properties = null) => {
+  amplitudeJS.getInstance().logEvent(eventName, properties);
+};
 
-export const track = (eventName, properties = null) => amplitude.track(eventName, properties);
+export const init = (currentUser) => {
+  const ampInstance = amplitudeJS.getInstance();
+  ampInstance.init(process.env.REACT_APP_AMPLITUDE_KEY);
+
+  if (currentUser) {
+    const userProperties = {
+      campusName: currentUser?.profile?.campus?.name,
+      email: currentUser?.profile?.email,
+      firstName: currentUser?.profile?.firstName,
+      lastName: currentUser?.profile?.lastName,
+      nickName: currentUser?.profile?.nickName,
+      userId: currentUser?.profile?.id,
+    };
+
+    amplitudeJS.getInstance().setUserId(currentUser?.profile?.id);
+
+    amplitudeJS.getInstance().setUserProperties(userProperties);
+  }
+
+  return null;
+};
+
+const amplitude = {
+  init,
+  trackEvent,
+};
+
+export default amplitude;
