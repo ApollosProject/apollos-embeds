94bb480e149ccc46d5afd373caffba19e38479c3
rajitharussel <138706838+rajitharussel@users.noreply.github.com>
2023-12-12 22:35:59 +0530

    feat: Added Static Link support on HorizontalCardListFeature.js HeroListFeature.js HorizontalMediaListFeature.js VerticalCardListFeature.js (#129)
    Co-authored-by: Russel Rajitha <russelrajitha@gmail.com>
    Co-authored-by: Jordan Wade <jjwade31@gmail.com>

--- packages/web-shared/components/FeatureFeed/Features/VerticalCardListFeature.js
+++ packages/web-shared/components/FeatureFeed/Features/VerticalCardListFeature.js
@@ -21,6 +21,10 @@ function VerticalCardListFeature(props = {}) {
   const [state, dispatch] = useModal();
 
   const handleActionPress = (item) => {
+    if (item.action === 'OPEN_URL'){
+      return window.open(getURLFromType(item.relatedNode), '_blank');
+    }
+
     if (searchParams.get('id') !== getURLFromType(item.relatedNode)) {
       dispatchBreadcrumb(
         addBreadcrumb({
