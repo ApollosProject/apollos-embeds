d1967e7a9717771c63d54f75bcc26ba4406db500
Brian Lin <brian@differential.com>
2023-12-14 16:01:19 -0500

    remove comment

--- packages/web-shared/components/ContentSingle.js
+++ packages/web-shared/components/ContentSingle.js
@@ -27,22 +27,6 @@ function ContentSingle(props = {}) {
   const [state, dispatch] = useModal();
   const parseHTMLContent = useHTMLContent();
 
-  // useEffect(() => {
-  //   const searchParam = window.location.search;
-  //   // Get the search parameter from the URL to determine if item is content single
-  //   if (searchParam && !searchParam.includes('FeatureFeed')) {
-  //     amplitude.trackEvent({
-  //       eventName: 'ContentSingle',
-  //       properties: {
-  //         itemId: props.data?.id,
-  //         parentId: props.data?.parentChannel?.id,
-  //         parentName: props.data?.parentChannel?.name,
-  //         title: props.data?.title,
-  //       },
-  //     });
-  //   }
-  // }, [window.location.search]);
-
   const invalidPage = !props.loading && !props.data;
 
   // Video details
