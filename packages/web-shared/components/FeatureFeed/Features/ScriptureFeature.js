9e249ddbf7fc51301b39355196d0011c4d71354c
Brian Lin <brian@differential.com>
2024-01-18 15:39:37 -0500

    strip out scriptures for build out

--- packages/web-shared/components/FeatureFeed/Features/ScriptureFeature.js
+++ packages/web-shared/components/FeatureFeed/Features/ScriptureFeature.js
@@ -5,41 +5,9 @@ import Styled from './ActionListFeature.styles';
 import { useNavigate } from 'react-router-dom';
 
 function ScriptureFeature(props = {}) {
-  const navigate = useNavigate();
+  console.log(props);
 
-  const handleActionPress = (item) => {
-    navigate({
-      pathname: '/',
-      search: `?id=${getURLFromType(item.relatedNode)}`,
-    });
-  };
-
-  if (props?.feature?.actions?.length === 0 || !props?.feature?.actions) {
-    return null;
-  }
-
-  return (
-    <Box>
-      <Box padding="xs" fontWeight="600" color="base.gray" id="results">
-        {props.feature.title || props.feature.subtitle}
-      </Box>
-      <Styled.List>
-        {props.feature?.actions?.map((item, index) => {
-          return (
-            <Styled.Wrapper key={index}>
-              <ResourceCard
-                title={item.title}
-                subtitle={item.subtitle}
-                leadingAsset={item?.image}
-                onClick={() => handleActionPress(item)}
-                background="none"
-              />
-            </Styled.Wrapper>
-          );
-        })}
-      </Styled.List>
-    </Box>
-  );
+  return <Box></Box>;
 }
 
 ScriptureFeature.propTypes = {
