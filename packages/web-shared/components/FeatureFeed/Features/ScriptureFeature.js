c869f3f82a2aaa26a4752798b408a157dd3daf5d
Brian Lin <brian@differential.com>
2024-01-18 16:25:18 -0500

    initial scripture preview

--- packages/web-shared/components/FeatureFeed/Features/ScriptureFeature.js
+++ packages/web-shared/components/FeatureFeed/Features/ScriptureFeature.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getURLFromType } from '../../../utils';
-import { systemPropTypes, ResourceCard, Box } from '../../../ui-kit';
-import Styled from './ActionListFeature.styles';
+import { systemPropTypes, Box } from '../../../ui-kit';
+import PropTypes from 'prop-types';
+import Styled from './ScriptureFeature.styles';
 import { useNavigate } from 'react-router-dom';
+import { ArrowsOutSimple } from 'phosphor-react';
 
 function ScriptureFeature(props = {}) {
-  console.log(props);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const scriptures = props?.feature?.scriptures;
+  const temp = scriptures[0];
+  console.log(scriptures);
 
-  return <Box></Box>;
+  const handleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <Styled.ScriptureItem isExpanded={isExpanded}>
+      <Styled.ScriptureItemHeader>
+        <Styled.ScriptureItemTitle>Acts 20</Styled.ScriptureItemTitle>
+        <Styled.ScriptureItemVerses>Verses 1-24</Styled.ScriptureItemVerses>
+      </Styled.ScriptureItemHeader>
+
+      <Styled.ScriptureItemText>{temp.text}</Styled.ScriptureItemText>
+      <Styled.ScriptureItemExpandButton onClick={handleExpand}>
+        <ArrowsOutSimple size={16} />
+      </Styled.ScriptureItemExpandButton>
+    </Styled.ScriptureItem>
+  );
 }
 
 ScriptureFeature.propTypes = {
   ...systemPropTypes,
+  isExpanded: PropTypes.bool,
 };
 
 export default ScriptureFeature;
