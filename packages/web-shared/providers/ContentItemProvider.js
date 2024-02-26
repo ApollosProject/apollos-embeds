167a02836e7e58c16f833fb04c8f10a325a92914
Vincent Wilson <vince@classyh.at>
2024-02-26 13:35:09 -0500

    Update packages/web-shared/providers/ContentItemProvider.js

--- packages/web-shared/providers/ContentItemProvider.js
+++ packages/web-shared/providers/ContentItemProvider.js
@@ -6,7 +6,6 @@ import { useContentItem } from '../hooks';
 function ContentItemProvider({ Component, options }) {
   const { loading, error, item } = useContentItem(options);
 
-  console.log(item, 'item', error);
 
   return <Component data={item} loading={loading} error={error} />;
 }
