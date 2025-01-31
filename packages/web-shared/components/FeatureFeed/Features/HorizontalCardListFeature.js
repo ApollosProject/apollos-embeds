5790479f0b912af5bd0d178afb514849c1149538
rajitharussel <138706838+rajitharussel@users.noreply.github.com>
2025-01-31 19:40:42 +0530

    (Liquid) Embed Styling Incorrect APO-1446 (#255)
    * (Liquid) Embed Styling Incorrect APO-1446
    
    * (Liquid) Embed Styling Incorrect APO-1446

--- packages/web-shared/components/FeatureFeed/Features/HorizontalCardListFeature.js
+++ packages/web-shared/components/FeatureFeed/Features/HorizontalCardListFeature.js
@@ -28,7 +28,7 @@ function HorizontalCardListFeature(props = {}) {
   const handleActionPress = useHandleActionPress();
   const handlePrimaryActionPress = useHandlePrimaryActionPress(props.feature);
 
-  if (props?.feature?.cards?.length === 0 || !props?.feature?.cards) {
+  if (!props?.feature?.cards?.length) {
     return null;
   }
 
@@ -48,7 +48,7 @@ function HorizontalCardListFeature(props = {}) {
           />
         ) : null}
       </Box>
-      {props?.feature?.primaryAction && props?.feature?.cards?.length <= 2 ? (
+      {props?.feature?.cards?.length <= 2 ? (
         <Box
           display="flex"
           justifyContent="center"
@@ -56,41 +56,28 @@ function HorizontalCardListFeature(props = {}) {
           mt="xs"
           mb={{ _: '0', md: 'l' }}
         >
-          {props?.feature?.cards?.length === 1 ? (
-            <ContentCard
-              key={props?.feature?.cards[0].title}
-              image={props?.feature?.cards[0].coverImage}
-              title={props?.feature?.cards[0].title}
-              summary={props?.feature?.cards[0].subtitle}
-              channelLabel={props?.feature?.cards[0]?.relatedNode?.parentItem?.title}
-              videoMedia={get(props?.feature?.cards[0], 'relatedNode?.videos[0]', null)}
-              onClick={() => handleActionPress(props?.feature?.cards[0])}
-              horizontal={true}
-            />
-          ) : (
-            <Box
-              display="grid"
-              gridGap="20px"
-              gridTemplateColumns={{
-                _: 'repeat(1, 1fr)',
-                md: 'repeat(2, 1fr)',
-              }}
-            >
-              {props?.feature?.cards?.map((item) => (
-                <ContentCard
-                  key={item.title}
-                  image={item.coverImage}
-                  title={item.title}
-                  channelLabel={item?.relatedNode?.parentItem?.title}
-                  summary={item.summary}
-                  onClick={() => handleActionPress(item)}
-                  videoMedia={get(item, 'relatedNode?.videos[0]', null)}
-                />
-              ))}
-            </Box>
-          )}
+          <Box
+            display="grid"
+            gridGap="20px"
+            gridTemplateColumns={{
+              _: 'repeat(1, 1fr)',
+              md: 'repeat(2, 1fr)',
+            }}
+          >
+            {props?.feature?.cards?.map((item) => (
+              <ContentCard
+                key={item.title}
+                image={item.coverImage}
+                title={item.title}
+                channelLabel={item?.relatedNode?.parentItem?.title}
+                summary={item.summary}
+                onClick={() => handleActionPress(item)}
+                videoMedia={get(item, 'relatedNode?.videos[0]', null)}
+              />
+            ))}
+          </Box>
         </Box>
-      ) : props?.feature?.cards?.length >= 1 ? (
+      ) : (
         <Carousel
           arrows={false}
           swipeable={true}
@@ -101,7 +88,7 @@ function HorizontalCardListFeature(props = {}) {
           customButtonGroup={<ButtonGroup />}
           renderButtonGroupOutside
         >
-          {props.feature?.cards?.map((item, index) => (
+          {props.feature?.cards?.map((item) => (
             <ContentCard
               key={item.title}
               image={item.coverImage}
@@ -114,18 +101,6 @@ function HorizontalCardListFeature(props = {}) {
             />
           ))}
         </Carousel>
-      ) : (
-        <Box width="100%" display="flex" justifyContent="center" pt="l" px="l" textAlign="center">
-          {props.feature.title === 'Continue Watching' ? (
-            <Box fontSize="16px" fontWeight="600" color="base.primary">
-              All caught up? Check out our other sections for more content!
-            </Box>
-          ) : (
-            <Box fontStyle="italic" fontSize="14px">
-              Sorry, there is no media available at this time.
-            </Box>
-          )}
-        </Box>
       )}
     </Box>
   );
